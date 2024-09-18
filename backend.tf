773fbbba842120d898476f9d122acca51010d75b
hamada-fatma <fatma.hamada@etudiant-isi.utm.tn>
2024-09-18 09:17:08 +0100

    Test changes for GitHub Actions

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-prod-bucket"
+    bucket         = "terraform-prod-bucket1"
     key            = "prod/terraform.tfstate"
     dynamodb_table = "terraform-lock-prod"
     region = "us-east-1"

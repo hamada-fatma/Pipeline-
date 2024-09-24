b03abe163a74eb331a7dd2a883e82101794597cc
hamada-fatma <fatma.hamada@etudiant-isi.utm.tn>
2024-09-24 18:28:20 +0100

    Issue4:Deploy the app to ECS

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-module "network" {
+ module "network" {
   source = "./modules/network"
 
 
@@ -40,3 +40,37 @@ module "ecs" {
   listener_ports = [80]
   
 }
+
+# # Module réseau
+# module "network" {
+#   source = "./modules/network"
+
+#   vpc_cidr_block     = var.vpc_cidr_block
+#   subnet1_cidr_block = var.subnet1_cidr_block
+#   subnet2_cidr_block = var.subnet2_cidr_block
+
+#   security_group_name = var.security_group_name
+#   ingress_port        = var.ingress_port
+#   egress_port         = var.egress_port
+# }
+
+# # Module ECS
+# module "ecs" {
+#   source = "./modules/ecs"
+
+#   # Variables réseau
+#   subnets = var.subnets
+#   vpc_id  = var.vpc_id
+
+#   # Variables IAM
+#   ecs_task_execution_role_name = var.ecs_task_execution_role_name
+#   ecs_task_role_name           = var.ecs_task_role_name
+#   execution_role_policy_arn    = var.execution_role_policy_arn
+
+#   # Variables Load Balancer
+#   load_balancer_name = var.load_balancer_name
+
+#   # Target Groups et Listeners
+#   target_groups   = var.target_groups
+#   listener_ports  = var.listener_ports
+# }

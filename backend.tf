cbf57a2bdeb5834d11d675e2a8acc07f98bd0bb2
Hamada Fatma <85518755+hamada-fatma@users.noreply.github.com>
2024-09-25 21:34:45 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 #backend
 terraform {
   backend "s3" {
-    bucket         = "terraform-prod-bucket"
+    bucket         = "terraform-prod-bucket1"
     key            = "prod/terraform.tfstate"
     dynamodb_table = "terraform-lock-prod"
     region = "us-east-1"

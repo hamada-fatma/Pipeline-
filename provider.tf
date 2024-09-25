4dc486a8557533d800e728f2b5c9694b3884c5cc
Hamada Fatma <85518755+hamada-fatma@users.noreply.github.com>
2024-09-25 09:08:35 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -17,9 +17,15 @@ provider "aws" {
 
 
 
- resource "aws_ecr_repository" "my_app_repo" {
-   name = "projet1/node-app"
- }
+resource "aws_ecr_repository" "service1" {
+  name = "service1"
+}
+
+resource "aws_ecr_repository" "service2" {
+  name = "service2"
+}
+
+
 
 
 

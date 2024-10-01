690d7ca6c649a09e98b02c60f1a87a820f16b421
Hamada Fatma <85518755+hamada-fatma@users.noreply.github.com>
2024-10-01 19:14:39 +0100

    Update variables.tf

--- modules/ecs/variables.tf
+++ modules/ecs/variables.tf
@@ -1,37 +1,88 @@
-variable "vpc_cidr_block" {
-  description = "The CIDR block for the VPC"
+variable "subnets" {
+  description = "List of subnets for the ECS service and Load Balancer"
+  type        = list(string)
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
   type        = string
-  default     = "10.0.0.0/16"
 }
-
-variable "subnet1_cidr_block" {
-  description = "The CIDR block for the first subnet"
+###############
+variable "ecs_task_execution_role_name" {
+  description = "IAM role name for ECS task execution"
   type        = string
-  default     = "10.0.1.0/24"
 }
 
-variable "subnet2_cidr_block" {
-  description = "The CIDR block for the second subnet"
+variable "ecs_task_role_name" {
+  description = "IAM role name for ECS tasks"
   type        = string
-  default     = "10.0.2.0/24"
 }
 
-variable "security_group_name" {
-  description = "The name of the ECS security group"
+variable "execution_role_policy_arn" {
+  description = "ARN of the managed policy for the ECS task execution role"
   type        = string
-  default     = "ecs_security_group"
+}
+
+variable "load_balancer_name" {
+  description = "Name of the Load Balancer"
+  type        = string
+}
+
+variable "target_groups" {
+  description = "List of Target Groups for the Load Balancer"
+  type        = list(object({
+    name     = string
+    port     = number
+    protocol = string
+  }))
+}
+
+variable "listener_ports" {
+  description = "List of listener ports for the Load Balancer"
+  type        = list(number)
+}
+variable "aws_region" {
+  description = "Région AWS"
+  type        = string
+  default     = "us-east-1"  # Facultatif si vous avez une région par défaut
+}
+variable "aws_account_id" {
+  description = "ID du compte AWS"
+  type        = string
 }
 
 
-variable "egress_port" {
-  description = "The egress port for the security group"
-  type        = number
-  default     = 0
-}
-
-# # Variable pour les ports d'ingress
+# Variable pour les ports d'ingress
 variable "ingress_ports" {
   description = "Liste des ports autorisés pour l'accès entrant"
   type        = list(number)
   default     = [80, 8080, 9090] # Ajoutez les ports nécessaires ici
+}
+
+#########
+variable "microservices" {
+  description = "Liste des microservices avec les informations nécessaires pour créer les définitions de tâches et services ECS"
+  type = list(object({
+    app_name       = string
+    container_port = number
+    host_port      = number
+    cpu            = number
+    memory         = number
+  }))
+  default = [
+    {
+      app_name       = "service1"
+      container_port = 8080
+      host_port      = 8080
+      cpu            = 256
+      memory         = 512
+    },
+    {
+      app_name       = "service2"
+      container_port = 9090
+      host_port      = 9090
+      cpu            = 256
+      memory         = 512
+    }
+  ]
 }

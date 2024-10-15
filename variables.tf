d7ea6a21385600e8534e897694350f617217f166
Hamada Fatma <85518755+hamada-fatma@users.noreply.github.com>
2024-10-15 20:01:10 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,57 @@
+# Network Module Variables
+variable "vpc_cidr_block" {
+  type = string
+}
+
+variable "subnet1_cidr_block" {
+  type = string
+}
+
+variable "subnet2_cidr_block" {
+  type = string
+}
+
+variable "security_group_name" {
+  type = string
+}
+
+variable "ingress_ports" {
+  type = list(number)
+}
+
+variable "egress_port" {
+  type = number
+}
+
+# ECS Module Variables
+variable "aws_account_id" {
+  type = string
+}
+
+variable "ecs_task_execution_role_name" {
+  type = string
+}
+
+variable "ecs_task_role_name" {
+  type = string
+}
+
+variable "execution_role_policy_arn" {
+  type = string
+}
+
+variable "load_balancer_name" {
+  type = string
+}
+
+variable "target_groups" {
+  type = list(object({
+    name     = string
+    port     = number
+    protocol = string
+  }))
+}
+
+variable "listener_ports" {
+  type = list(number)
+}

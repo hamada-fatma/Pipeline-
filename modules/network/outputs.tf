c2b8c03cebe5efaf84d60de9d5c74aeab4b5d1f2
Hamada Fatma <85518755+hamada-fatma@users.noreply.github.com>
2024-10-17 18:30:08 +0100

    Update outputs.tf

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,10 +1,3 @@
-# output "vpc_id" {
-#   value = aws_vpc.this.id
-# }
-
-# output "subnet_ids" {
-#   value = [aws_subnet.this1.id, aws_subnet.this2.id]
-# }
 output "vpc_id" {
   value = aws_vpc.this.id
 }
